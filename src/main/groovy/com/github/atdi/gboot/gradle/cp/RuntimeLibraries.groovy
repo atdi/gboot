7677534f6417482442b0b6ac7a463cce9b2aa85c
atdi <aurel.avramescu@gmail.com>
2015-03-24 19:10:56 +0100

    Start the TDD

--- src/main/groovy/com/github/atdi/gboot/gradle/cp/RuntimeLibraries.groovy
+++ src/main/groovy/com/github/atdi/gboot/gradle/cp/RuntimeLibraries.groovy
@@ -1,11 +1,24 @@
 package com.github.atdi.gboot.gradle.cp
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ResolvedArtifact
 
 /**
  * Copyright (C) 2015 Aurel Avramescu
  */
 class RuntimeLibraries {
 
-    private final Project project;
+    private Project project
+
+    private Set<ResolvedArtifact> getLibraries(String configurationName) {
+        Configuration configuration = (configurationName == null ? null : this.project
+                .getConfigurations().findByName(configurationName))
+        if (configuration == null) {
+            return null;
+        }
+        return configuration.getResolvedConfiguration()
+                .getResolvedArtifacts()
+
+    }
 }
--- src/test/groovy/com/github/atdi/gboot/gradle/cp/RuntimeLibrariesTest.groovy
+++ src/test/groovy/com/github/atdi/gboot/gradle/cp/RuntimeLibrariesTest.groovy
@@ -1,15 +1,43 @@
 package com.github.atdi.gboot.gradle.cp
 
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ResolvedArtifact
+import org.gradle.testfixtures.ProjectBuilder
+
 /**
  * Copyright (C) 2015 Aurel Avramescu
  */
 class RuntimeLibrariesTest extends GroovyTestCase {
+
+    RuntimeLibraries libraries;
+
     void setUp() {
         super.setUp()
-
+        Project project = setupProject()
+        libraries = new RuntimeLibraries(project: project)
     }
 
     void tearDown() {
 
+    }
+
+    void testRuntimeLibraries() {
+        Set<ResolvedArtifact> libs = libraries.getLibraries("compile")
+        assertEquals(1, libs.size())
+    }
+
+
+    private setupProject() {
+        Project project = ProjectBuilder.builder()
+                .withName('test-project')
+                .build()
+        project.apply plugin: 'com.github.atdi.gboot'
+        project.apply plugin: 'java'
+        project.dependencies {
+            compile("org.eclipse.jetty:jetty-server:9.2.10.v20150310")
+            testCompile("junit:junit:4.12")
+        }
+
+        return project
     }
 }
