c7eff251d46dd0d289eb0c77c6454667ad4edd08
aav <aav@marketlogicsoftware.com>
2015-03-24 16:05:53 +0100

    Add libraries management classes

--- src/main/groovy/com/github/atdi/gboot/gradle/cp/RuntimeLibraries.groovy
+++ src/main/groovy/com/github/atdi/gboot/gradle/cp/RuntimeLibraries.groovy
@@ -1,7 +1,11 @@
 package com.github.atdi.gboot.gradle.cp
 
+import org.gradle.api.Project
+
 /**
  * Copyright (C) 2015 Aurel Avramescu
  */
 class RuntimeLibraries {
+
+    private final Project project;
 }
