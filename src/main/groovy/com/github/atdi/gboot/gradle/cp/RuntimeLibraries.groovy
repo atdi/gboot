519644fb302f99711d9db381184c4189eb1f08a0
aav <aav@marketlogicsoftware.com>
2015-03-24 09:41:29 +0100

    Add libraries management classes

---
+++ src/main/groovy/com/github/atdi/gboot/gradle/cp/RuntimeLibraries.groovy
@@ -0,0 +1,7 @@
+package com.github.atdi.gboot.gradle.cp
+
+/**
+ * Copyright (C) 2015 Aurel Avramescu
+ */
+class RuntimeLibraries {
+}
---
+++ src/test/groovy/com/github/atdi/gboot/gradle/cp/RuntimeLibrariesTest.groovy
@@ -0,0 +1,15 @@
+package com.github.atdi.gboot.gradle.cp
+
+/**
+ * Copyright (C) 2015 Aurel Avramescu
+ */
+class RuntimeLibrariesTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void tearDown() {
+
+    }
+}
