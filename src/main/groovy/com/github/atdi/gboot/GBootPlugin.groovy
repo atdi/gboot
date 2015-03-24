20008a5084d4d84421df2b686816205b19670b6f
atdi <aurel.avramescu@gmail.com>
2015-03-24 07:25:34 +0100

    Initial commit

---
+++ src/main/groovy/com/github/atdi/gboot/GBootPlugin.groovy
@@ -0,0 +1,15 @@
+package com.github.atdi.gboot
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by aurel on 3/23/15.
+ */
+class GBootPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
