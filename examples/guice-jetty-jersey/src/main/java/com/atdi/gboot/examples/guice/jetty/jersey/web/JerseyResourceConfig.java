554143bc39eb2a43b90618f53d1243c7d580619f
aav <aav@marketlogicsoftware.com>
2015-04-16 15:13:53 +0200

    #11: Refactor example for starters

--- examples/guice-jetty-jersey/src/main/java/com/atdi/gboot/examples/guice/jetty/jersey/web/JerseyResourceConfig.java
+++ examples/guice-jetty-jersey/src/main/java/com/atdi/gboot/examples/guice/jetty/jersey/web/JerseyResourceConfig.java
@@ -10,19 +10,12 @@ import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
 
 import javax.inject.Inject;
 
-public class JerseyResourceConfig extends ResourceConfig {
+public class JerseyResourceConfig extends AbstractResourceConfig {
 
     @Inject
     public JerseyResourceConfig(ServiceLocator serviceLocator) {
+        super(serviceLocator, new PersistenceModule("demo-guice-boot"));
         // Set package to look for resources in
         packages("com.atdi.gboot.examples.guice.jetty.jersey.resources");
-
-        //log.info("Registering injectables...");
-
-        GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
-
-        GuiceIntoHK2Bridge guiceBridge = serviceLocator.getService(GuiceIntoHK2Bridge.class);
-        guiceBridge.bridgeGuiceInjector(Guice.createInjector(new PersistenceModule("demo-guice-boot")));
-
     }
 }
